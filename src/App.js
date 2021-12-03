8ff7945fd338bd896a47f3ee4d8d5d7eb48fac8d
Oleg Sozonik <sozonik999@gmail.com>
2021-12-04 01:51:52 +0300

    fix: fix

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import Map from "../src/components/map/Map";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Map />
-    </div>
-  );
+  return <div className="App">LOL</div>;
 }
 
 export default App;
